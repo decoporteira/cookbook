571e1806fc8b78c68d524a0530e4583829a66580
decoporteira <andreporteira@gmail.com>
2023-12-13 11:27:56 -0300

    API pronta

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::RecipesController < Api::V1::ApiController
+
+    def show
+        recipe = Recipe.find(params[:id])
+       
+        render status: 200, json: recipe.as_json(expect: [:created_at, :updated_at]).merge(user_mail: recipe.user.email)
+    end
+
+end
---
+++ spec/requests/api/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Mostrar detalhes da receita' do
+    context 'Get /api/v1/recipes/id' do
+        it 'sucess' do
+            #arrange 
+            user = create(:user, email: 'user@email.com', password: '123456', role: :admin)
+            recipe_type = create(:recipe_type, name: 'Lanche')
+            recipe = Recipe.create(title: 'Dobradinha', cook_time: 12, recipe_type_id: recipe_type.id, ingredients: 'Pão e ovo', instructions: 'Misturar tudo na panela', user_id: user.id )
+            Category.create(name: 'Regional', user_id: user.id)
+
+            #act
+            get "/api/v1/recipes/#{recipe.id}"
+            
+            #Assert
+            expect(response.status).to eq 200
+            expect(response.content_type).to include 'application/json'
+
+            json_response = JSON.parse(response.body)
+            expect(json_response['title']).to eq('Dobradinha')
+            expect(json_response['cook_time']).to eq(12)
+            expect(json_response['ingredients']).to eq('Pão e ovo')
+            expect(json_response['instructions']).to eq('Misturar tudo na panela')
+            expect(json_response['user_mail']).to eq("#{user.email}")
+
+
+        end
+    end
+
+end
